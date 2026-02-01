08cd1b349596ef9e6f70ef65dc37d1695fc6fe7a
AamirrH <aamirr.1704@gmail.com>
2026-02-02 01:06:58 +0530

    add necessary dependencies

---
+++ src/test/java/com/code/security/app/securitywebapp/SecurityWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.security.app.securitywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
